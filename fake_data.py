47bf88903fbce42c67a16566592e72027a627e4b
Rick Shapiro <rshaprio@better.com>
2021-08-10 18:10:03 -0400

    missing step in generating fake data + comments

--- fake_data.py
+++ fake_data.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import itertools as it
 
+# python fake_data.py
+
 df = (
     pd.read_csv('static/market_data.csv', parse_dates=['Date'])
       .loc[lambda f: f.Region.ne('all_regions'), ['Date', 'Brand', 'Region', 'Spend']]
@@ -11,21 +13,18 @@ df = (
       .assign(Spend= lambda f: f.Spend.map(lambda s: max(s, 0)))
 )
 
-ix = np.concatenate([pd.date_range(df.Date.min(), df.Date.max(), 200).floor('d').values for _ in range(5)])
-df['Week'] = ix
 
 # curve
 idx = list(it.product(df.Brand.unique(), df.Region.unique()))
 
-# Hierarchical for `a` param
-# for brand
+# means for each brand
 brand_mn = stats.expon(loc=200, scale=500).rvs(df.Brand.nunique())
-# for each region
+# b0 for mm curve for each region (but pulled from the distribution for the corresponding brand so they are correlated)
 a = stats.norm(loc=brand_mn, scale=50).rvs([4,5]).T.ravel()
 
-# rest
+# rate parameters are random
 b = stats.norm(loc=500, scale=50).rvs(len(idx))
-# for each val
+# fuc to generate some noise for each value 
 c = stats.halfnorm(loc=50, scale=25).rvs
 
 coefs = zip(a,b)
@@ -37,5 +36,16 @@ for i, cat in enumerate(idx):
     # df.loc[mask, 'Sales'] = a[i]*df.loc[mask, 'Spend']**b[i] + c(mask.sum())
     # mm
     df.loc[mask, 'Sales'] = (a[i] * df.loc[mask, 'Spend'])/(b[i] + df.loc[mask, 'Spend']) + c(mask.sum())
+
+# Ultimately I figured breaking it out by region would be making it more complicated than an intro presentation should be
+#+ and also not enough time so I'm gonna just pretend this is brand only data...
+#+ The resulting data will look like it has different "groupings" which an astute practitioner might cluster/relabel and would find they get better models.
+
+# Re-index the dates so they fit in the same space (eg it's monthly data with four rows per brand per date so make it weekly data with one row per brand per date)
+ix = np.concatenate([pd.date_range(df.Date.min(), df.Date.max(), 200).floor('d').values for _ in range(5)])
+df['Week'] = ix
+
     
-df.to_csv('static/market_data_final.csv', index=False)
+df.drop("Region",1).to_csv('static/market_data_final.csv', index=False)
+
+
